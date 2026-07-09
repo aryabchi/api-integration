278b911621f89c12b610f2c144afdb69c16ca40d
Anton Ryabchikov <ryabchikovap@bsc.sibur.ru>
2026-07-09 11:44:13 +0300

    Tested fresh install, added comments to example.py

--- scripts/example.py
+++ scripts/example.py
@@ -16,14 +16,18 @@ Prerequisites:
        .\.venv\Scripts\Activate.ps1
 
     2. Latest .whl must be installed IN THAT virtual env
-       # deps from Internet
+       # dependencies from Internet
        pip install api_integration-<latest-tag-version>-py3-none-any.whl
-       # OR deps from local dir (no Internet)
+       # OR dependencies from local dir (no Internet)
        pip install --no-index --find-links=.\dist --find-links=.\dist\dependencies api_integration
+       # if installing new version run first
+       pip uninstall api_integration -y
 
     3. .env file with `APP_ENV=prod` and valid credentials must exist in the project root (./.env file)
+       `MAIL_SERVER=INTERNET` connects to Yandex mail servers, may fail from inside SIBUR net
+       `MAIL_SERVER=CORPORATE` connects to MS Exchange (in autodiscover mode), needs valid `EXCHANGE_` credentials, may fail
 
-    4. ./config/trusted_recipients.json must exist
+    4. ./config/trusted_recipients.json must exist, fix mail addresses for testing
 
     5. example.py must exist in ./scripts/example.py
 
@@ -34,11 +38,11 @@ Usage:
     # Windows PowerShell:
     python .\scripts\example.py
 
-    # Or using the installed console script (if package is installed):
-    run-api-integration
-
     # Or without virtual env pre-activation:
     .\.venv\Scripts\python .\scripts\example.py
+
+    # Or using the installed console .exe script:
+    run-api-integration
 """
 
 from api_integration.pipeline import run_pipeline_with_lock
